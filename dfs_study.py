381524b425f1a734fcb85f577cc8691aabf6e8ac
taeyoungkim-dev <goldrunty@gmail.com>
2026-02-04 21:11:23 +0900

    Add DFS algorithm implementation and update main execution flow

---
+++ dfs_study.py
@@ -0,0 +1,26 @@
+import const
+import map_manager
+START_POS = [0,0]
+class Dfs:
+    def __init__(self,map_manager:map_manager.MapManager):
+        self.__que = [START_POS]
+        self.__path_list = []
+        self.__move = {const.LEFT:(0,-1),const.RIGHT:(0,1),const.UP:(1,0),const.DOWN:(-1,0)}
+        self.__map_manager = map_manager
+    def one_step(self):
+        if len(self.__que)==0 and len(self.__path_list)==0:
+            return const.STUCK
+        print(f"DEBUG: 현재 큐 사이즈 = {len(self.__que)}")
+        current_pos = self.__que.pop()
+        self.__path_list.append(current_pos)
+        for move,dif in self.__move.items():
+            row,col = current_pos[0]+dif[0],current_pos[1]+dif[1]
+            if not self.__map_manager.is_out_of_map([row,col]) :
+                if not self.__map_manager.is_wall([row,col]):
+                    if self.__map_manager.is_game_end([row,col]):
+                        return const.FOUND
+                    elif not [row,col] in self.__path_list and not [row,col] in self.__que :
+                        self.__que.append([row,col])
+        return const.SEARCHING
+    def get_path_list(self):
+        return [coordinate for coordinate in self.__path_list]
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import map_manager
 import map_renderer
 import bfs_study
+import dfs_study
 import os
 import time
 import const
 if __name__ == "__main__" :
     map_m = map_manager.MapManager()
     map_r = map_renderer.MapRenderer()
-    bfs = bfs_study.Bfs(map_m)
+    #algo = bfs_study.Bfs(map_m)
+    algo = dfs_study.Dfs(map_m)
+    #algo = astar_study.Astar(map_m)
     while True:
         os.system('clear')
-        status = bfs.one_step()
-        map_r.print_path(map_m,bfs.get_path_list())
+        status = algo.one_step()
+        map_r.print_path(map_m,algo.get_path_list())
         if status == const.FOUND:
             print("FOUND")
             break
         elif status == const.STUCK:
             print("STUCK")
             break
+        time.sleep(0.2)
     #한발짝 가기
     #print
     #기다리기
